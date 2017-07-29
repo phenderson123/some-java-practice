bcdccb4d038c01b595631a25f79ec8ed9e7b55d6
Perry E. Henderson <hendersonperry96@yahoo.com>
2017-07-29 09:14:47 -0400

    first commit

---
+++ one.java
@@ -0,0 +1,16 @@
+class DataTypes
+{
+		public static void main ( String[] args) {     }
+}
+
+char letter = 'N';
+String title = "The sky is blue" ;
+int number = 365;
+float decimal = 98.6f;
+boolean result = true ;
+
+System.out.println("initial is"+ letter);
+System.out.println("Book name is" + title);
+System.out.println("Number of days is"+ number);
+System.out.println("Temp is"+ decimal);
+System.out.println("Answer is" + result);
